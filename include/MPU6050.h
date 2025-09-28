813f10cb5c789cc3ee533ad944157f394d0a4b29
mauicv <alexander.athorne@gmail.com>
2025-09-28 18:00:27 +0100

    Integrate first pass at mpu6050

---
+++ include/MPU6050.h
@@ -0,0 +1,115 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+
+
+#include <iostream>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#include <cstdint>
+
+#include <string>
+using std::string;
+
+
+extern "C" {
+	#include <linux/i2c-dev.h>
+	#include <i2c/smbus.h>
+}
+
+std::string toBinaryString(uint8_t byte) {
+    std::string bits;
+    bits.reserve(8);
+
+    for (int i = 7; i >= 0; --i) {
+        bits.push_back((byte & (1 << i)) ? '1' : '0');
+    }
+    return bits;
+}
+
+uint8_t read(int f_dev, uint8_t reg) {
+    int32_t ret = i2c_smbus_read_byte_data(f_dev, reg);
+    if (ret < 0) {
+        std::cout << "Error reading from MPU6050\n";
+        return 0;
+    }
+    uint8_t byte = static_cast<uint8_t>(ret);
+    return byte;
+}
+
+uint8_t write(int f_dev, uint8_t reg, uint8_t val) {
+    int32_t ret = i2c_smbus_write_byte_data(f_dev, reg, val);
+    if (ret < 0) {
+        std::cout << "Error writing to MPU6050\n";
+        return 0;
+    }
+    return 1;
+}
+
+bool read_block(int f_dev, uint8_t reg, uint8_t* data, uint8_t length) {
+    int32_t ret = i2c_smbus_read_i2c_block_data(f_dev, reg, length, data);
+    if (ret < 0) {
+        std::cout << "Error reading from MPU6050\n";
+        return false;
+    }
+    return true;
+}
+
+class MPU6050 {
+public:
+    int f_dev;
+    float data[6];
+
+    MPU6050() {
+        f_dev = open("/dev/i2c-1", O_RDWR);
+        std::cout << "f_dev: " << f_dev << std::endl;
+        if (f_dev < 0) {
+            std::cout << "Failed to open /dev/i2c-1\n";
+        }
+
+        if (ioctl(f_dev, I2C_SLAVE, 0x68) < 0) {
+            std::cout << "Failed to set I2C slave address\n";
+        }
+
+        uint8_t ret = read(f_dev, 0x75);
+        std::cout << toBinaryString(ret) << std::endl;
+
+        write(f_dev, 0x6B, 0x00);
+        std::cout << "Verify write to 0x6B(Non-sleep mode): ";
+        ret = read(f_dev, 0x6B);
+        std::cout << (ret & (1 << 2)) << std::endl;
+
+        std::cout << "Verify 2g scale: ";
+        ret = read(f_dev, 0x1C);
+        std::cout << (ret & (1 << 4)) << (ret & (1 << 3)) << std::endl;
+
+        std::cout << "Verify 250dps scale: ";
+        ret = read(f_dev, 0x1B);
+        std::cout << (ret & (1 << 4)) << (ret & (1 << 3)) << std::endl;
+
+        std::cout << "Config Output: ";
+        ret = write(f_dev, 0x1A, 0b00000011);
+        ret = read(f_dev, 0x1A);
+        std::cout << toBinaryString(ret) << std::endl;
+    }
+
+    void get_sensor_data(double dt) {
+        uint8_t sensor_data[14];
+        read_block(f_dev, 0x3B, sensor_data, 14);
+
+        data[0] = (float)((sensor_data[0] << 8) | sensor_data[1])/16384;
+        data[1] = (float)((sensor_data[2] << 8) | sensor_data[3])/16384;
+        data[2] = (float)((sensor_data[4] << 8) | sensor_data[5])/16384;
+
+        float temp = (float)((sensor_data[6] << 8) | sensor_data[7])/340 + 36.53;
+        
+        data[3] = (float)((sensor_data[8] << 8) | sensor_data[9])/131;
+        data[4] = (float)((sensor_data[10] << 8) | sensor_data[11])/131;
+        data[5] = (float)((sensor_data[12] << 8) | sensor_data[13])/131;
+    }
+
+    float* get_data() {
+        return data;
+    }
+};
+#endif // MPU6050_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,58 @@
 #include "scheduled_executor.h"
 #include "servo.h"
 #include <vector>
-
+#include "MPU6050.h"
 
 int main() {
-    int pi = pigpio_start(nullptr, nullptr);
-    if (pi < 0) {
-        std::cerr << "Failed to connect to pigpiod\n";
-        return 1;
-    }
+    // int pi = pigpio_start(nullptr, nullptr);
+    // if (pi < 0) {
+    //     std::cerr << "Failed to connect to pigpiod\n";
+    //     return 1;
+    // }
 
-    std::vector<Servo> servo_list;
-    servo_list.emplace_back(pi, "servo1", 17, 0.08, 0.01, 0.005, 0, 0.2, -1, 0);
-    servo_list.emplace_back(pi, "servo2", 27, 0.08, 0.01, 0.005, 0, 0.7, -1, 0);
-    ServoGroup servos(servo_list);
+    // std::vector<Servo> servo_list;
+    // servo_list.emplace_back(pi, "servo1", 17, 0.08, 0.01, 0.005, 0, 0.2, -1, 0);
+    // servo_list.emplace_back(pi, "servo2", 27, 0.08, 0.01, 0.005, 0, 0.7, -1, 0);
+    // ServoGroup servos(servo_list);
 
-    ScheduledExecutor executor(
+    // ScheduledExecutor executor(
+    //     [&](double dt) {
+    //         servos.update_values(dt);
+    //         return true;
+    //     },
+    //     0.001
+    // );
+
+    // executor.start();
+    // servos.update_setpoints({0, 0});
+    // // for (int i = 0; i < 3; i++) {
+    // //     servos.update_setpoints({0, 0});
+    // //     std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    // //     servos.update_setpoints({-0.4, -0.2});
+    // //     std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    // // }
+    // std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    // pigpio_stop(pi); // disconnect from daemon
+    // executor.stop();
+
+    MPU6050 mpu6050;
+    ScheduledExecutor mpu6050_executor(
         [&](double dt) {
-            servos.update_values(dt);
+            mpu6050.get_sensor_data(dt);
             return true;
         },
         0.001
     );
+    mpu6050_executor.start();
 
-
-    executor.start();
-    for (int i = 0; i < 3; i++) {
-        servos.update_setpoints({0, 0});
-        std::this_thread::sleep_for(std::chrono::seconds(2)); 
-        servos.update_setpoints({-0.4, -0.2});
-        std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    for (int i = 0; i < 100; i++) {
+        float* data = mpu6050.get_data();
+        std::cout << "acc: " << data[0] << " " << data[1] << " " << data[2] << std::endl;
+        std::cout << "gyro: " << data[3] << " " << data[4] << " " << data[5] << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    pigpio_stop(pi); // disconnect from daemon
-    executor.stop();
 
+    mpu6050_executor.stop();
     return 0;
 }
 
